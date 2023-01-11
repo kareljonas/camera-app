812a05313277baa0f1dc4b7fbfb2075971e00f7f
kareljonas <112886149+kareljonas@users.noreply.github.com>
2023-01-11 07:51:59 +0100

    kj first edit
    kj upravy

--- part-1/app.js
+++ part-1/app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: "environment" }, audio: false };
 var track = null;
 
 // Define constants
@@ -26,10 +26,46 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    //console.log(cameraSensor.toDataURL())
+    cameraOutput.src = cameraSensor.toDataURL("image/webp"); //"image/webp"
     cameraOutput.classList.add("taken");
+    var blob = dataURItoBlob(cameraOutput.src);
+    console.log('kj after onclick'+blob);
     // track.stop();
+    //Then just append the blob to a new FormData object and post it to your server using ajax:
+    var fd = new FormData(document.forms[0]);
+    var xhr = new XMLHttpRequest();
+    fd.append("KJmyFile", blob);
+    xhr.open('POST', '/', true);
+    xhr.send(fd);
+    var file = new File([blob], "KJfilenam", { type: "image/jpeg"});
 };
+
+/*this will convert a dataURI to a Blob:-*/
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
+//From there, appending the data to a form such that it will be uploaded as a file is easy:
+
+//var dataURL = canvas.toDataURL('image/jpeg', 0.5);
+//var blob = dataURItoBlob(dataURL);
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
